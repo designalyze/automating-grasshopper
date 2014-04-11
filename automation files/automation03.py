2d8362ffcb0d740ce65b5d668ea2b6d6a61d4a04
Designalyze <zach@designalyze.com>
2014-04-11 16:25:30 -0400

    Removed file specific GUIDs

--- automation files/automation03.py
+++ automation files/automation03.py
@@ -16,14 +16,14 @@ rs.EnableRedraw(True)
  
 for i in range(1,6,1):
     #BaseSize Slider
-    gh.SetSliderValue("adf54977-006a-4877-9e67-81d736ff07de",i)
+    gh.SetSliderValue("GUID_FOR_SLIDER_1",i)
     for j in range(0,11,1):
         #Rotation Slider
-        gh.SetSliderValue("061dd51d-3d85-4a32-8aaa-0e6ba809ed75",j)
+        gh.SetSliderValue("GUID_FOR_SLIDER_2",j)
         for k in range(3,7,1):
             #NumberOfSides Slider
-            gh.SetSliderValue("b46e0b0e-643d-4b3a-8a96-c6d4e120e4dd",k)
-            gh.RunSolver("TowerOptionsAutomation.gh")
-            baked = gh.BakeDataInObject("72628cf1-020e-425d-9746-6f323f8882f8")
+            gh.SetSliderValue("GUID_FOR_SLIDER_3",k)
+            gh.RunSolver("FILENAME.gh")
+            baked = gh.BakeDataInObject("GUID_FOR_BAKE_OBJECT")
             transVect = (12*i, 12*j, 12*(k-2))
             rs.MoveObject(baked,transVect)
